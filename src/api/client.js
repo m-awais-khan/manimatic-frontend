801ef613ba779d148ecb90bbe6002df60f5cc3a7
Awais Khan <468awaiskhan2003@gmail.com>
2026-07-27 17:27:11 +0500

    Fix trailing slash double URL bug

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL 
-    ? `${import.meta.env.VITE_API_BASE_URL}/api/`
+    ? `${import.meta.env.VITE_API_BASE_URL.replace(/\/+$/, '')}/api/`
     : 'http://localhost:8000/api/';
 
 const api = axios.create({
